f186999ee536c30be6dea8ed2c250993744328e4
Juanca9915 <62212476+JuanCamilo9915@users.noreply.github.com>
2021-07-22 21:45:32 -0500

    Se crea la carpeta proyects-Udemy, donde reposan 3 caprteas, las cuales contiene los proyectos desarrollados en los IDEs de Neatbeans, Apache Neatbeans e Intelliji.

---
+++ Aprendizaje-Guia/Proyects-Udemy/Proyectos-en-Apache-Neatbeans/AppHolaMundo/src/main/java/HolaMundo.java
@@ -0,0 +1,14 @@
+/**
+ * @author JuanCamiloDev
+ */
+public class HolaMundo {
+    public static void main(String[] args) {
+        //Atributos de la clase:
+        String cadena = "Hola Mundo en Java!, utilizando una variable de tipo cadena";
+        //var despedida = "Hasta pronto amigo"; --> Esta opción solo esta disponible para las versiones del jdk 10 en adelante
+        
+        System.out.println(cadena);
+        System.out.println(cadena);
+        System.out.println(cadena);
+    }
+}
